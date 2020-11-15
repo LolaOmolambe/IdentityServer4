c4088561192df4bfad04a0288bf5dbbea8d9ed08
Omolola <lola.omolambe@gmail.com>
2020-11-15 18:20:39 +0100

    Add project files.

---
+++ Identity.ConsoleClient/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Identity.ConsoleClient
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+        private static async Task MainAsync()
+        {
+            //get all the endpoints of identity server
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if(disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            //Grab bearer token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "IdentityApi"
+            });
+
+            if(tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            //Consume User Api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var customerInfo = new StringContent(
+                JsonConvert.SerializeObject(
+                    new { Id = 1, FirstName = "Lola", LastName = "Omolambeee" }
+                ), Encoding.UTF8, "application/json");
+
+            var createUser = await apiClient.PostAsync("http://localhost:53409/api/users", customerInfo);
+
+            if(createUser.IsSuccessStatusCode)
+            {
+                Console.WriteLine(createUser.StatusCode);
+            }
+
+            var getUser = await apiClient.GetAsync("http://localhost:53409/api/users"); 
+            if(!getUser.IsSuccessStatusCode)
+            {
+                Console.WriteLine(getUser.StatusCode);
+            }
+            else
+            {
+                var content = await getUser.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Identity.IdentityServer/Config.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("IdentityApi", "User Api for Identity")
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("IdentityApi", "User Api for Identity")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "IdentityApi" }
+                }
+            };
+        }
+    }
+}
